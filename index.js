054e67fc411a728eb9b9e1ac8b6e02943e0ec8fa
lukagvenetadze <lukagvenetadze04@gmail.com>
2024-10-30 01:35:40 +0400

    Created js file

---
+++ index.js
@@ -0,0 +1,13 @@
+let firstName = "Luka";
+let lastName = "Gvenetadze";
+let age = 24;
+let email = "lukagvenetadze04@gmail.com";
+let phoneNumber = 555432657;
+let isTeenage = true;
+
+console.log(
+  `%c${firstName} ${lastName} ${age} ${email} %c${phoneNumber} %c${isTeenage}`,
+  "color: white",
+  "color: red",
+  "color: yellow"
+);
